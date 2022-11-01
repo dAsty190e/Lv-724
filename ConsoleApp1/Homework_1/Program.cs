28c29f49c0919b237b7a2728534cb0bc80f28e7b
dAsty190e <m.lesya11@gmail.com>
2022-10-31 22:29:23 -0400

    Homework_1 and Taks_1

---
+++ ConsoleApp1/Homework_1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define integer variable a (side of the square)
+
+            Console.WriteLine("Lets find out what is the sides of the square");
+            Console.WriteLine("\nHow long is one side of the square?");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nThe perimeter is {a * 4}");
+            Console.WriteLine($"\nThe area is {a * a}");
+
+            // Define string name and integer age
+
+            string userInput;
+            string name;
+            int age;
+
+            Console.WriteLine("\nWhat is your name?");
+            userInput = Console.ReadLine();
+            name = userInput;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine($"It is wonderful {name}?");
+            }
+
+            var isParsing = false;
+            do
+            {
+            Console.WriteLine($"\nHow old are you {name}?");
+            userInput = Console.ReadLine();
+            isParsing = int.TryParse(userInput, out age);
+
+            } while (isParsing == false);
+
+            Console.WriteLine($"{name} your age is {age}");
+
+
+            // Read double number r (radius of a circle).
+            // Calculate the length(l = 2 * pi * r), area(S = pi * r * r), and volume(4 / 3 * pi * r * r * r) of a circle
+
+            Console.WriteLine("\nGive me the radius of a circle");
+            string userRadius = Console.ReadLine();
+
+            double r = double.Parse(userRadius);
+            var length = 2 * Math.PI * r;
+            var area = Math.PI * r * r;
+            var volume = 4 / 3 * Math.PI * r * r * r;
+
+            Console.WriteLine($"Length : {length}");
+            Console.WriteLine($"Area : {area}");
+            Console.WriteLine($"Volume : {volume}");
+        }
+    }
+}
---
+++ ConsoleApp1/Task_1/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define integer variables a and b.
+            Console.WriteLine("Lets us calculate numbers");
+            Console.WriteLine("Put your number a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Put your number b: ");
+            int b = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine($"Your addition number is: {a} + {b} = " + (a + b));
+            Console.WriteLine($"Your subtraction number is: {a} - {b} = " + (a - b));
+            Console.WriteLine($"Your multiplication number is: {a} * {b} = " + (a * b));
+            Console.WriteLine($"Your divide number is: {a} / {b} = " + (a / b));
+
+
+            // Output question “How are you?“
+
+            Console.WriteLine("\nWhat is your name?");
+            var userInput = Console.ReadLine();
+            var name = userInput;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine($"\nNice to meet you {name}");
+                Console.WriteLine($"\nHow are you {name}? a/b");
+                Console.WriteLine("\n a.Good");
+                Console.WriteLine("\n b.Not so good");
+                char answer = Console.ReadKey().KeyChar;
+                _ = Console.ReadLine();
+
+
+                if (char.ToLower(answer) == 'a')
+                {
+                    Console.WriteLine($"\nGlad to hear it!");
+                }
+                else if (char.ToLower(answer) == 'b')
+                {
+                    Console.WriteLine($"\nHope your day will be better!");
+                }
+            }
+
+            // Read 3 variables of char type
+
+            Console.WriteLine("\nDo you want choose a pet! y/n");
+            var petChoice = Console.ReadKey().KeyChar;
+
+            if (char.ToLower(petChoice) == 'y')
+            {
+                Console.WriteLine("\nWhat pet do you like? a/b/c");
+                Console.WriteLine("\n a.Dog");
+                Console.WriteLine("\n b.Cat");
+                Console.WriteLine("\n c.Perrot");
+
+                var petInput = Console.ReadLine();
+                petInput = petInput.ToLower();
+
+                if (petInput == "a")
+                {
+                    Console.WriteLine($"\nIt is your dog now {name}");
+                }
+                else if (petInput == "b")
+                {
+                    Console.WriteLine($"\nIt is your cat now {name}");
+                }
+                else if (petInput == "c")
+                {
+                    Console.WriteLine($"\nPerrot is yours now {name}");
+                }
+
+            }
+            else if (char.ToLower(petChoice) == 'n')
+            {
+                Console.WriteLine("\nSee you next time!");
+            }
+            Console.WriteLine("");
+
+            // Enter 2 integer numbers
+            // Check if they are both positive – use bool expression
+
+            Console.WriteLine("Numbers positive/negative check");
+            Console.WriteLine("Put your number A: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Put your number B: ");
+            int d = int.Parse(Console.ReadLine());
+
+            if ((c == d))
+            {
+                Console.WriteLine("They equal to each other");
+            }
+            
+            if ((c < 0) && (d > 0))
+            {
+                Console.WriteLine("A is negative and B is positive");
+            }
+            
+            if ((c > 0) && (d < 0))
+            {
+                Console.WriteLine("A is positive and B is negative");
+            }
+
+            if ((c > 0) && (d > 0))
+            {
+                Console.WriteLine("They both positive");
+            }
+
+            if ((c < 0) && (d < 0))
+            {
+                Console.WriteLine("They both negative");
+            }
+
+
+            Console.WriteLine("\n");
+        }
+    }
+}
