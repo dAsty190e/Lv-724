03e6a14ba79a6fa1a37b14e523a9f59a57389f79
Dima <58148797+Dima-212@users.noreply.github.com>
2022-11-07 15:07:34 +0200

    Homework2.Dima

--- Program.cs
+++ Program.cs
@@ -1,36 +1,70 @@
 ﻿using System;
 
-namespace HW1_Stanislav_Cherniak
+namespace Homework2.Dima
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			//Task 1
-			Console.WriteLine("Please, input the side of the square: ");
-			var a = Convert.ToInt32(Console.ReadLine());
-			var perimeter = a + a + a + a;
-			var area = a * a;
-			Console.WriteLine("Perimeter = " + perimeter);	
-			Console.WriteLine("Area = " + area);
-			Console.ReadKey();
-			//Task 2
-			Console.WriteLine("What is your name?");
-			string name = Console.ReadLine();
-			Console.WriteLine($"How old are you {name}?");
-			int age = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine(name + " your age is " + age + " years");
-			Console.ReadKey();
-			//Task 3
-			Console.WriteLine("Please, input number ");
-			double r = Convert.ToDouble(Console.ReadLine());
-			var length = 2 * Math.PI * r;
-			var square = Math.PI * r * r;
-			var volume = 4 / 3 * Math.PI * r * r * r;
-			Console.WriteLine("Length of circle is " + length);
-			Console.WriteLine("Square of circle is " + square);
-			Console.WriteLine("Volume of circle is " + volume);
-			Console.ReadKey();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+            Console.WriteLine("Task 1");
+            Console.WriteLine("Enter 3 numbers");
+            string first = Console.ReadLine();
+            float number1 = float.Parse(first);
+            string second = Console.ReadLine();
+            float number2 = float.Parse(second);
+            string third = Console.ReadLine();
+            float number3 = float.Parse(third);
+            if (number1 >= -5 && number1 <= 5)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            if (number2 >= -5 && number1 <= 5)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            if (number3 >= -5 && number1 <= 5)
+                Console.WriteLine("yes");
+            else
+                Console.WriteLine("no");
+            Console.ReadKey();
+
+            //task 2
+            Console.WriteLine("Task 2");
+            Console.WriteLine("Enter 3 numbers");
+            string four = Console.ReadLine();
+            int number4 = Convert.ToInt32(four);
+            string five = Console.ReadLine();
+            int number5 = Convert.ToInt32(five);
+            string six = Console.ReadLine();
+            int number6 = Convert.ToInt32(six);
+            if (number4 > number5 && number4 > number5)
+                Console.WriteLine("number one is the biggest");
+            else if (number5 > number4 && number5 > number6)
+                Console.WriteLine("number two is the biggest ");
+            else
+                Console.WriteLine("number three is the biggest");
+
+            //task 4
+            Console.WriteLine("Task 4");
+            MyDog mydog;
+            mydog.name = "Rex";
+            mydog.mark = "dachshund";
+            mydog.age = 2;
+
+            mydog.info();
+
+        }
+    }
+    struct MyDog
+    {
+        public string name;
+        public string mark;
+        public int age;
+
+        public void info()
+        {
+            Console.WriteLine($"my dog name is {name}, mark is {mark} it is {age} years old");
+        }
+    }
 }
