fa84d23dc7f92d2b08c48949433e62d1de867464
amoreCaro <117117217+amoreCaro@users.noreply.github.com>
2022-11-10 00:11:55 +0200

    Add files via upload

---
+++ Lesson1/Classwork_1/Classwork_1/Classwork_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classwork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Double a, b;
+            Console.Write("Enter a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            //Output
+            Console.WriteLine("Sum {0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("Sub  " + a + "-" + b + "= " + (a - b));
+            Console.WriteLine($"Mult {a} * {b} = {a * b}");
+            Console.WriteLine("{0} / {1} = {2:F2}", a, b, a / b);
+            Console.ReadKey();
+        }
+    }
+}
