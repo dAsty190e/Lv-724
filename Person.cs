bafe7fea9d1504159052e88c8e2badee2fcc6e98
amoreCaro <117117217+amoreCaro@users.noreply.github.com>
2022-11-16 19:08:49 +0200

    HW_4

---
+++ Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Lesson4
+{
+    public class Person
+    {      
+            private string name;
+            public string Name
+            {
+                set { name = value; }
+                get { return name; }
+            }
+            private int birthYear;
+            public int BirthYear
+            {
+                set { birthYear = value; }
+                get { return birthYear; }
+            }
+            public int age;
+            public Person()
+            {
+                name = "Vlad";
+                birthYear = 2007;
+            }
+            public Person(string name, int birthYear)
+            {
+                this.name = name;
+                this.birthYear = birthYear;
+            }
+
+            public static Person Input(int i)
+            {
+                Console.WriteLine($"Enter {i + 1} your name");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Enter {i + 1} your year of birth");
+                string birthYear1 = Console.ReadLine();
+                int birthYear = Convert.ToInt32(birthYear1);
+                Person person = new Person(name, birthYear);
+                return person;
+            }
+            public void Age()
+            {
+                var today = DateTime.Today;
+                age = today.Year - birthYear;
+            }
+            public void ChangeName()
+            {
+                if (age < 16)
+                {
+                    name = "Very Young";
+                }
+            }
+            public void Output()
+            {
+                Console.WriteLine($"the name is {name} the birth of year is {birthYear}, the age is {age}");
+                Console.WriteLine("________________");
+            }
+
+            public static bool operator == (Person first, Person second)
+            {
+                return (first.name == second.name);
+            }
+            public static bool operator !=(Person first, Person second)
+            {
+                return !(first == second);
+            }
+            public override string ToString()
+            {
+                return $"the name is {name} the birth of year is {birthYear}, the age is {age}";
+            }
+        }
+}
--- Program.cs
+++ Program.cs
@@ -1,71 +1,51 @@
 ﻿using System;
 
-namespace HomeWork4.Dima
+namespace Lesson4
 {
-    class Person
+    class Program
     {
-        private string name;
-        public string Name
+        static void Main(string[] args)
         {
-            set { name = value; }
-            get { return name; }
-        }
-        private int birthYear;
-        public int BirthYear
-        {
-            set { birthYear = value; }
-            get { return birthYear;  }
-        }
-        public int age;
-        public Person()
-        {
-            name = "Dima";
-            birthYear = 2001;
-        }
-        public Person(string name, int birthYear)
-        {
-            this.name = name;
-            this.birthYear = birthYear;
-        }
-        
-        public static Person Input(int i)
-        {
-            Console.WriteLine($"Enter {i + 1} your name");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Enter {i + 1} your year of birth");
-            string birthYear1 = Console.ReadLine();
-            int birthYear = Convert.ToInt32(birthYear1);
-            Person person = new Person(name, birthYear);
-            return person;
-        }
-        public void Age()
-        {
-            var today = DateTime.Today;
-            age = today.Year - birthYear;
-        }
-        public void ChangeName()
-        {
-            if (age < 16)
+            Person person = new Person();
+            person.Output();
+
+            Person person2 = new Person("fjkvfdv", 2002);
+            person2.Output();
+            Person[] persons = new Person[6];
+            persons[0] = person;
+            persons[1] = person2;
+            for (int i = 2; i < persons.Length; i++)
             {
-                name = "Very Young";
+                persons[i] = Person.Input(i);
             }
-        }
-        public void Output()
-        {
-            Console.WriteLine($"the name is {name} the birth of year is {birthYear}, the age is {age}");
-        }
-       
-        public static bool operator == (Person first, Person second)
-        {
-            return (first.name == second.name);
-        }
-        public static bool operator != (Person first, Person second)
-        {
-            return !(first == second);
-        }
-        public override string ToString()
-        {
-            return $"the name is {name} the birth of year is {birthYear}, the age is {age}";
+            for (int i = 0; i < persons.Length; i++)
+            {
+                persons[i].Age();
+                persons[i].Output();
+            }
+            string newName = "Very Young";
+            for (int i = 0; i < persons.Length; i++)
+            {
+                if (persons[i].age < 16)
+                {
+                    persons[i].Name = newName;
+                    Console.WriteLine($"people number {i + 1} has new name - {newName}");
+                }
+            }
+            for (int i = 0; i < persons.Length; i++)
+            {
+                for (int j = i + 1; j < persons.Length; j++)
+                {
+                    if (persons[i] == persons[j])
+                    {
+                        Console.WriteLine($"people number {i + 1} and {j + 1}  have the same name");
+                    }
+                }
+            }
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
         }
     }
 }
