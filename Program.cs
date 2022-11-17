a68ce0926a43c34f4c1a80c98eba807624aaa1df
Ga11ardo <90717318+Ga11ardo@users.noreply.github.com>
2022-11-17 02:15:06 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,71 +1,119 @@
 ﻿using System;
 
-namespace HomeWork4.Dima
+namespace Sytnyk_Illia_s_Homework_4
 {
     class Person
     {
         private string name;
-        public string Name
-        {
-            set { name = value; }
-            get { return name; }
-        }
-        private int birthYear;
-        public int BirthYear
-        {
-            set { birthYear = value; }
-            get { return birthYear;  }
-        }
-        public int age;
-        public Person()
-        {
-            name = "Dima";
-            birthYear = 2001;
-        }
-        public Person(string name, int birthYear)
+        private DateTime birthYear;
+
+        public Person() { }
+        
+        public Person(string name, DateTime birthYear)
         {
             this.name = name;
             this.birthYear = birthYear;
         }
-        
-        public static Person Input(int i)
+
+        public string Name
         {
-            Console.WriteLine($"Enter {i + 1} your name");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Enter {i + 1} your year of birth");
-            string birthYear1 = Console.ReadLine();
-            int birthYear = Convert.ToInt32(birthYear1);
-            Person person = new Person(name, birthYear);
-            return person;
+            get { return name; }
+            set { name = value; }
         }
-        public void Age()
+
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+            set { birthYear = value; }
+        }
+
+        public int Age()
         {
             var today = DateTime.Today;
-            age = today.Year - birthYear;
+            int age = today.Year - birthYear.Year;
+            return age;
         }
-        public void ChangeName()
+
+        public void Input()
         {
-            if (age < 16)
-            {
-                name = "Very Young";
-            }
+            Console.WriteLine("Enter the name of a person: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the birthday year of a person: ");
+            birthYear = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("------------------------------");
         }
-        public void Output()
+
+        public string ChangeName(string new_name)
         {
-            Console.WriteLine($"the name is {name} the birth of year is {birthYear}, the age is {age}");
+            name = new_name;
+            return name;
         }
-       
-        public static bool operator == (Person first, Person second)
-        {
-            return (first.name == second.name);
-        }
-        public static bool operator != (Person first, Person second)
-        {
-            return !(first == second);
-        }
+
         public override string ToString()
         {
-            return $"the name is {name} the birth of year is {birthYear}, the age is {age}";
+            return $"Person name is {name} and birthday year is {birthYear.Year}";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator == (Person first, Person second)
+        {
+            return first.name == second.name;
+        }
+
+        public static bool operator !=(Person first, Person second)
+        {
+            return first.name != second.name;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p_1 = new Person("Illya", new DateTime (2003, 07, 24));
+            Person p_2 = new Person("Volodymyr", new DateTime(1978, 01, 25));
+            Person p_3 = new Person("Petro", new DateTime(1965, 09, 26));
+            Person p_4 = new Person("Valeriy", new DateTime(1973, 07, 08));
+            Person p_5 = new Person("Olena", new DateTime(1978, 02, 06));
+            Person p_6 = new Person("Yulia", new DateTime(1960, 11, 27));
+
+            Person[] people = { p_1, p_2, p_3, p_4, p_5, p_6 };
+            Console.WriteLine("------------------------------");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine($"Person {i+1} is {people[i].Age()} years old");
+            }
+
+            for(int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Age() < 16)
+                {
+                    people[i].ChangeName("Very Young");
+                }
+            }
+            Console.WriteLine("------------------------------");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i].Output();
+            }
+            Console.WriteLine("------------------------------");
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                for (int j = i+1; j < people.Length; j++)
+                {
+                    if(people[i] == people[j])
+                    {
+                        Console.WriteLine($"Names of {i+1} person and {j+1} person are the same");
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
