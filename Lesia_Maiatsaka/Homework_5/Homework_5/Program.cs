9d1858850ab101ce65b45369bb7d6f2c6327378d
dAsty190e <m.lesya11@gmail.com>
2022-11-19 16:29:23 -0500

    Lesson_5, Homework_5, Task_5

---
+++ Lesia_Maiatsaka/Homework_5/Homework_5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var developers = new List<IDeveloper>();
+            developers.Add(new Builder { Tool = "Crane" });
+            developers.Add(new Builder { Tool = "Shovel" });
+            developers.Add(new Builder { Tool = "Hammer" });
+            developers.Add(new Programmer { Tool = "C#" });
+            developers.Add(new Programmer { Tool = "C++" });
+            developers.Add(new Programmer { Tool = "Html"});
+
+            foreach (var developer in developers) 
+            {
+                Console.WriteLine(developer.Create());
+                Console.WriteLine(developer.Destroy());
+            }
+
+            developers.Sort();
+            Console.WriteLine();
+            foreach (var developer in developers)
+            {
+                Console.WriteLine(developer.Create());
+                Console.WriteLine(developer.Destroy());
+            }
+
+            // linq
+            // developers = developers.OrderBy(i => i.Tool).ToList();
+        }
+    }
+}
---
+++ Lesia_Maiatsaka/Homework_5/Homework_5/Programmer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+    public class Programmer : IDeveloper
+    {
+        private string _language;
+        public string Tool { get { return _language; } set { _language = value; } }
+
+        public int CompareTo(IDeveloper? other)
+        {
+            if (other == null) return 1;
+            return Tool.CompareTo(other.Tool);
+        }
+
+        public string Create()
+        {
+            return $"Create programmer {_language}";
+        }
+
+        public string Destroy()
+        {
+            return "Destroy programmer";
+        }
+    }
+}
