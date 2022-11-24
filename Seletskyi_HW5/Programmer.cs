980d959635f545153ac012681e8afa8bcd69f8be
√42 <102050884+sqrt42@users.noreply.github.com>
2022-11-24 16:22:42 +0200

    Homework 5

---
+++ Seletskyi_HW5/Programmer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seletskiy_HW5
+{
+    class Programmer : IDeveloper, IComparable<IDeveloper>
+    {
+        private string language;
+
+        private string profession;
+
+        public string Tool
+        {
+            get
+            {
+                return this.language;
+            }
+
+            set
+            {
+                this.language = value;
+            }
+        }
+
+        public string Profession
+        {
+            get
+            {
+                return this.profession;
+            }
+
+            set
+            {
+                this.profession = value;
+            }
+        }
+
+        public Programmer(string lang, string prof)
+        {
+            this.language = lang;
+            this.profession = prof;
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("{0} wrote some code using {1}", this.profession, this.language);
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine("{0} deleted a project written in {1}", this.profession, this.language);
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+    }
+}
