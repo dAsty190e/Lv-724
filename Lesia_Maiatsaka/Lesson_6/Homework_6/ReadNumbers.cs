5cf28bf8d023e8dddda854ff7c816f012658f179
dAsty190e <m.lesya11@gmail.com>
2022-11-26 19:38:34 -0500

    This will  add console app for read numbers This will set _current to start on ReadNumbers instantiation This will add complete as termination check This will Track ValidNumbers

--- Lesia_Maiatsaka/Lesson_6/Homework_6/ReadNumbers.cs
+++ Lesia_Maiatsaka/Lesson_6/Homework_6/ReadNumbers.cs
@@ -10,15 +10,24 @@
         {
             _start = start;
             _end = end;
+            _current = start;
+            ValidNumbers = new List<int>();
         }
+
+        public List<int> ValidNumbers { get; set; }
 
         public int ReadNumber(int number) 
         {
-            if (number < _start || number > _end || number < _current)
+            if (number < _start || number > _end || number <= _current)
             {
                 throw new Exception("Number is invalid");
             }
+            ValidNumbers.Add(number);
             return _current = number;
+        }
+        public bool Complete(int number) 
+        {
+            return number == _end;
         }
     }
 }
