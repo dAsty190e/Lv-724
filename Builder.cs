1fe3964e117de8034ad17e124da72b278cbe7c60
Dima <58148797+Dima-212@users.noreply.github.com>
2022-12-01 16:54:57 +0200

    HomeWork5.Dima

---
+++ Builder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HM5
+{
+    class Builder : IDeveloper, IComparable
+    {
+        string tool;
+        public string Tool
+        {
+            set { tool = value; }
+            get { return tool; }
+        }
+
+        public Builder(string tool)
+        {
+            this.tool = tool;
+        }
+        public static Builder Input(int i)
+        {
+            Console.WriteLine($"{i + 1} enter your tool");
+            string tool = Console.ReadLine();
+            Builder builder = new Builder(tool);
+            return builder;
+        }
+        public void Create()
+        {
+            Console.WriteLine($"The tool is {this.tool}");
+        }
+        public void Destroy()
+        {
+            if (tool != "hammer")
+            {
+                Console.WriteLine("your language isn't c# ");
+            }
+            else
+            {
+                Console.WriteLine("youк tool isn't hammer");
+            }
+        }
+        public Builder() { }
+        public int CompareTo(object o)
+        {
+            Builder b = o as Builder;
+            if (b != null)
+                return this.Tool.CompareTo(b.Tool);
+            else
+                throw new ArgumentException("Object is not a developer");
+        }
+    }
+}
