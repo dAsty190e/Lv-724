a23acd50e2238ed7a6d29441424353619fb1889a
dAsty190e <m.lesya11@gmail.com>
2022-12-05 11:49:39 -0500

    Calculator + CalculatorTest

---
+++ Lesia_Maiatsaka/CalculatorTest/Calculator/ClassCalculator.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator
+{
+    public class ClassCalculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multyply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
