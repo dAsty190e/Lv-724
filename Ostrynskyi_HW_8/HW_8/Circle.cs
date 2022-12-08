5897621b04dbf60270953a3416af2a12a7af0cfb
Vlad_is_love <106085859+Vladisloveua@users.noreply.github.com>
2022-12-08 10:59:24 +0200

    Add files via upload

---
+++ Ostrynskyi_HW_8/HW_8/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ostrynskyi_HW_8
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public double Radius { get { return radius; } }
+
+        public Circle(string name, double radius) : base(name)
+        {
+            this.radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * (radius * radius);
+        }
+        public override double Perimeter()
+        {
+            return Math.PI * 2 * radius;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {name}, Area: {Area()}, Perimetr: {Perimeter()}");
+        }
+    }
+}
---
+++ Ostrynskyi_HW_8/Task_8/Developer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ostrynskyi_task_8
+{
+    public class Developer : Staff, IComparable<Persons>
+    {
+        private string level;
+        public Developer(string name, string level, int salary) : base(name, salary)
+        {
+            this.level = level;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("{0} is a {1} developer with salary {2}", Name, level, Salary);
+        }
+    }
+}
