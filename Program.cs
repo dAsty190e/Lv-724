d2f94a1f61818f16ecfde551488f3adb111a994d
amoreCaro <117117217+amoreCaro@users.noreply.github.com>
2022-12-09 13:59:52 +0200

    HW&Task6

--- Program.cs
+++ Program.cs
@@ -1,50 +1,38 @@
-
-﻿using System;
-using System.Collections.Generic;
-
-namespace Task_5
+﻿namespace Homework_6
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            var flyable = new List<IFlyable>();
-            flyable.Add(new Bird("King Penguin", false));
-            flyable.Add(new Bird("Amazon Parrot", true));
-            flyable.Add(new Bird("American Crow", true));
-            flyable.Add(new Plane("Beechcraft Baron", 123));
-            flyable.Add(new Plane("Gulfstream G450", 654));
-            flyable.Add(new Plane("Boing", 999));
+            bool control;
+            do
+            {
+                Console.WriteLine("Give me two int numb:");
+                Console.WriteLine("First number:");
+                var userInput = int.Parse(Console.ReadLine());
+                Console.WriteLine("Second number:");
+                var userInput2 = int.Parse(Console.ReadLine());
 
-            foreach (var flyableObjects in flyable)
-            {
-                Console.WriteLine(flyableObjects.Fly());
-            }
- 
-            Dictionary<uint, string> catalogue = new Dictionary<uint, string>();
-            catalogue.Add(11, "Julia");
-            catalogue.Add(12, "Ivan");
-            catalogue.Add(13, "Dima");
-            catalogue.Add(14, "Petro");
-            catalogue.Add(15, "Ivanka");
-            catalogue.Add(16, "Zorana");
-            catalogue.Add(17, "Veronika");
-            Console.WriteLine("Enter the ID");
-            string numbers = Console.ReadLine();
-            uint number = Convert.ToUInt32(numbers);
-            foreach (char c in catalogue.Keys)
-            {
-                if (c == number)
+                var numbers = new Hw_Math();
+                try
                 {
-                    Console.WriteLine($"The name is {catalogue[c]}");
+                    var result = numbers.Div(userInput, userInput2);
+                    control = false;
                 }
-                else if (c != number)
+                catch (DivideByZeroException ex)
                 {
-                    Console.WriteLine("No name with such ID");
+                    Console.WriteLine("Cant be divided by 0");
+                    Console.WriteLine(ex);
+                    Console.WriteLine("Try again");
+                    control = true;
                 }
-                //Я зробив, що на всі інші ID пише "No name with such ID", а щоб просто виводилось, якщо нема такого ID не додумався
-            }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong, try again");
+                    throw;
+                }
+            } while (control);
+
         }
     }
 }
-
